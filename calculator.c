626e7255ce986e0bc5f6ff974493a48df7952604
Rishabh Yadav <117223377+rishabh7923@users.noreply.github.com>
2024-01-09 23:57:43 +0530

    Update calculator.c
    replaced if with switch

--- calculator.c
+++ calculator.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main() {
     char operator;
@@ -10,11 +9,14 @@ int main() {
     printf("Expression: ");
     scanf("%f %c %f", &n1, &operator, &n2);
 
-    result = (operator == '+') 
-        ? n1 + n2 : (operator== '-')
-        ? n1 - n2 : (operator== '/')
-        ? n1 / n2 : (operator== '*')
-        ? n1 * n2 : (printf("Invalid operator, please try again with valid operator"), exit(1), 0);
+    switch (operator) {
+        case '+': result = n1 + n2; break;
+        case '-': result = n1 - n2; break;
+        case '/': result = n1 / n2; break;
+        case '*': result = n1 * n2; break;
+        default:
+            return printf("\nInvalid operator, please try again with valid operator");
+    }
 
     printf("Result: %f\n\n", result);
 }
